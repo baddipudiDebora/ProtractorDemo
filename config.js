1655d722d83616467a74fa67d31207ec5a4637c5
swaroopdebora <kumar11.2k@gmail.com>
2020-12-22 15:26:18 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.config = {
+	//seleniumAddress: 'http://localhost:4444/wd/hub',
+	directConnect: true,
+	specs : [ 'spec2.js' ],
+
+	
+
+};
+
---
+++ spec1.js
@@ -0,0 +1,15 @@
+// describe is Test Suite -- means a feature to be tested
+
+
+// describe takes 2 parameters , name of teh senario and a function
+describe('Protractor Demo App', function() {
+
+    browser.get('http://www.angularjs.org');
+      
+  });
+  // describe is eqaul to test senraion --> each features
+  describe('Login feature', function() {
+    it('test with valid', function() { 
+       
+  });
+});
